19f7472f8a0ecff6b035586a0ac502265cb3a22c
cdragon-1 <streetfood89@gmail.com>
2017-03-18 20:49:13 +0900

    Overriding Generic View Methods

--- django_app/courses/views.py
+++ django_app/courses/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
+from rest_framework import generics
 
 from . import models
 from . import serializers
-
 
 
 class ListCreateCourse(generics.ListCreateAPIView):
@@ -14,3 +14,28 @@ class ListCreateCourse(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyCourse(generics.RetrieveDestroyAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+
+
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+    def perform_create(self, serializer):
+        course = get_object_or_404(
+            models.Course, pk=self.kwargs.get('course_pk'))
+        serializer.save(course=course)
+
+
+class RetrieveUpdateDestroyReview(generics.RetrieveDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            course_id=self.kwargs.get('course_pk'),
+            pk=self.kwargs.get('pk')
+        )
