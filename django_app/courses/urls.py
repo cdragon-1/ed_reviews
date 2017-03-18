e7d967c1f8fcee9c01d85709f19b963e83f01b51
cdragon-1 <streetfood89@gmail.com>
2017-03-18 20:20:21 +0900

    Generic CRUD Reformatting views and connected urls

--- django_app/courses/urls.py
+++ django_app/courses/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ListCourse.as_view(), name='course_list'),
+    url(r'^$', views.ListCreateCourse.as_view(), name='course_list'),
+    url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyCourse.as_view(), name='course_detail'),
 ]
--- django_app/courses/views.py
+++ django_app/courses/views.py
@@ -1,13 +1,16 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+
 
 from . import models
 from . import serializers
 
 
 
-class ListCourse(APIView):
-    def get(self, request, format=None):
-        courses = models.Course.objects.all()
-        serializer = serializers.CourseSerializer(courses, many=True)
-        return Response(serializer.data)
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
+
+
+class RetrieveUpdateDestroyCourse(generics.RetrieveDestroyAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
